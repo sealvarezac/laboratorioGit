d2e7016a5868b6ca244c8778488919256b9a7719
Estudiante <jusosaa@unal.edu.co>
2026-05-06 08:49:29 -0500

    agregan lineas al .txt y getline e ifstream al .cpp

--- labgit.cpp
+++ labgit.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
-
+#include <fstream>
+#include <string>
 using namespace std;
+
 int main(){
-	
+	ifstream archivo_git ("./input/archivoEntrada.txt");
+	if (archivo_git.is_open()){
+		string linea;
+		getline(archivo_git, linea);
+		while(getline(archivo_git, linea)){
+            cout<<linea<<endl;
+        }
+	}
+    cout<<"Se leyo el archivo de forma correcta."<<endl<<endl;
+	archivo_git.close();
+	cout<<endl;
+	return 0;
 }
