d3980c8000ba096e62251d403ed01298769e5856
sibustamantec <sibustamantec@unal.edu.co>
2026-05-06 21:48:03 -0500

    Se agrego archivo de salida dentro de carpeta output

--- labgit.cpp
+++ labgit.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 int main(){
 	ifstream archivo_git ("./input/archivoEntrada.txt");
-	if (archivo_git.is_open()){
+	ofstream archivoSalida("./output/salida.txt");
+	if (archivo_git.is_open() && archivoSalida.is_open()){
 		string linea;
-		getline(archivo_git, linea);
 		while(getline(archivo_git, linea)){
-            cout<<linea<<endl;
-        }
+            archivoSalida<<linea<<"\n";
+        }archivoSalida<<"Salida correcta.\n";
 	}
     cout<<"Se leyo el archivo de forma correcta."<<endl<<endl;
 	archivo_git.close();
